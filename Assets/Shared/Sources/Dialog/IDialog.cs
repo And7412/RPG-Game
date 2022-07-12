d7fda1ff69ebe2fb52cc56651392753353c6a06e
Ivan Grigorev <absolute1405@gmail.com>
2022-07-12 20:03:31 +0300

    IDialog

---
+++ Assets/Shared/Sources/Dialog/IDialog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace RPG.Shared.Dialog
+{
+    public interface IDialog<T,V> where T: DialogArgs where V: DialogResult
+    {
+        event Action<V> Closed;
+        void Open(T args);
+    }
+
+    public class DialogArgs { }
+    public class DialogResult { }
+}
