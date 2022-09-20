e954738947056ee10b3e5b84159cd7e5ab6e8b23
And7412 <90712704+And7412@users.noreply.github.com>
2022-09-20 20:06:20 +0300

    venderDialog

---
+++ Assets/Shared/Sources/Dialog/VenderDialog.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Shared.Dialog;
+using RPG.PlayerSystem;
+namespace RPG.Vendors
+{
+    public class VendorDialog : IDialog<VendorDialogArgs>
+    {
+
+    }
+    public class VendorDialogArgs : DialogArgs
+    {
+        private VendorConfig _config;
+        private IPlayerTrade _playermoney;
+        public VendorDialogArgs (VendorConfig config,IPlayerTrade player)
+        {
+            _config = config;
+            _playermoney = player.Money;
+        }
+    }
+    public class VendorDialogResult : DialogResult
+    {
+
+    }
+}
---
+++ Assets/Vendors/Sources/Vendor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace RPG.Vendors
+{
+    public class Vendor : MonoBehaviour
+    {
+        [SerializeField] private Button _button;
+        private void Awake()
+        {
+            _button.onClick.AddListener(OnClick);
+        }
+        private void OnClick()
+        {
+
+        }
+    }
+
+}
