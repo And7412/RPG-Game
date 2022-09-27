68de90a7268c8fe324f4b73ac08afe3542996ec6
Ivan Grigorev <absolute1405@gmail.com>
2022-09-27 19:20:49 +0300

    Inventory item added

---
+++ Assets/Item/Sources/InventoryItem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using RPG.Item;
+using UnityEngine;
+
+namespace RPG.InventorySystem
+{
+    public class InventoryItem
+    {
+        public ItemConfig Config { get; }
+        public int Amount { get; private set; }
+
+        public InventoryItem(ItemConfig config, int amount)
+        {
+            Config = config;
+            Amount = amount;
+        }
+
+        public void Add(int amount)
+        {
+            Amount += amount;
+        }
+
+        public void Remove(int amount)
+        {
+            Amount -= amount;
+        }
+    }
+}
+
---
+++ Assets/Item/Sources/ItemInventoryView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using RPG.Item;
+using UnityEngine;
+
+namespace RPG.InventorySystem
+{
+    public class ItemInventoryView : MonoBehaviour
+    {
+        public event Action<> 
+        private Item _config;
+
+        public void SetConfig(ItemConfig config)
+        {
+            _config = config;
+        }
+    }
+}
+
