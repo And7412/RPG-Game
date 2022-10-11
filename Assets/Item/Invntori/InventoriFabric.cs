f3dcd1eb11a3181460ada9bf29720a770590b35a
And7412 <90712704+And7412@users.noreply.github.com>
2022-10-11 16:31:45 +0300

    Inventori Update

---
+++ Assets/Item/Invntori/InventoriFabric.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace RPG.InventorySystem
+{
+
+    public class InventoriFabric : MonoBehaviour
+    {
+        [SerializeField] private ItemInventoryView _item;
+        [SerializeField] private RectTransform _root;
+
+        public ItemInventoryView Create(InventoryCell cell)
+        {
+            var item = Instantiate(_item, _root);
+            _item.SetCell(cell);
+            return item;
+        }
+    }
+}
---
+++ Assets/Item/Invntori/InventoriPool.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace RPG.InventorySystem
+{
+    public class InventoriPool
+    {
+        private List<GameObject> _items;
+
+        public InventoriPool(GameObject[] item)
+        {
+            foreach (var obj in item)
+            {
+                _items.Add(obj.gameObject);
+            }
+            _items = new List<GameObject>(item);
+        }
+
+        public GameObject Get()
+        {
+            foreach (var obj in _items)
+            {
+                if (obj.activeSelf == false)
+                {
+                    obj.SetActive(true);
+                    return obj;
+                }
+            }
+            throw new InvalidOperationException("no items in pull");
+        }
+
+        public void Push(GameObject obj)
+        {
+            obj.SetActive(false);
+        }
+    }
+}
