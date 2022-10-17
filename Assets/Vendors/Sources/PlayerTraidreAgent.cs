f6ad5ce7214f14ba71b6388425f082bd4d4c5c8f
And7412 <90712704+And7412@users.noreply.github.com>
2022-10-17 12:37:07 +0300

    H/W (2/3)

---
+++ Assets/Vendors/Sources/PlayerTraidreAgent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.InventorySystem;
+using RPG.Vendors;
+using RPG.PlayerSystem;
+namespace RPG.TradeSystem
+{
+    public class PlayerTraidreAgent : MonoBehaviour
+    {
+        [SerializeField] private Vendor _vendor;
+        [SerializeField] private Inventory _playerInvetory;
+        [SerializeField] private VendorDialog _vendorDialog;
+        private PlayerMoney _money;
+
+        private void Awake()
+        {
+            _vendor.GiveInventoy += GiveInventoy;            
+        }
+        private void GiveInventoy(Inventory inventoyVendor)
+        {
+            var args = new VendorDialogArgs(_money.Value, _playerInvetory, inventoyVendor);
+            _vendorDialog.Open(args);
+        }
+    }
+}
--- Assets/Vendors/Sources/Vendor.cs
+++ Assets/Vendors/Sources/Vendor.cs
@@ -2,18 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+using RPG.InventorySystem;
 namespace RPG.Vendors
 {
     public class Vendor : MonoBehaviour
     {
         [SerializeField] private Button _button;
+        [SerializeField] private Inventory _inventory;
+
+        public event Action<Inventory> GiveInventoy;
+
         private void Awake()
         {
             _button.onClick.AddListener(OnClick);
         }
         private void OnClick()
         {
-
+            GiveInventoy?.Invoke(_inventory);
         }
     }
 
