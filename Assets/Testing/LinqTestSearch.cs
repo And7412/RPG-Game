69a0ee5d27a4b9cb85840ef7fef5b534821a8317
And7412 <90712704+And7412@users.noreply.github.com>
2022-11-08 19:41:30 +0300

    linq test

--- Assets/Testing/LinqTestSearch.cs
+++ Assets/Testing/LinqTestSearch.cs
@@ -30,7 +30,7 @@ namespace Testing
            _tests.Add(new LinqTest(0, "F"));
            _tests.Add(new LinqTest(0, "Foo"));
 
-           FindWhere();
+            SelectIds();
         }
 
         public void FindWhere()
@@ -42,6 +42,25 @@ namespace Testing
                 Debug.Log(item.Id);
             }
         }
+        public void GetCount()
+        {
+            int result = _tests.Count(x => x.Number==2);
+            Debug.Log(result);
+        }
 
+        public void AnyFoo()
+        {
+            bool result = _tests.Any(x => x.Id == "Foo");
+            Debug.Log(result);
+        }
+        public void SelectIds()
+        {
+            IEnumerable<string> result = _tests.Select(x=> x.Id );
+
+            foreach (var item in result)
+            {
+                Debug.Log(item);
+            }
+        }
     }
 }
