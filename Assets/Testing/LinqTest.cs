b66a378fba40a871b2b2e859df0cfab46ae6cc86
And7412 <90712704+And7412@users.noreply.github.com>
2022-11-08 18:44:43 +0300

    h/w

---
+++ Assets/Testing/LinqTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Testing
+{
+    public class LinqTest
+    {
+        private int _munber;
+        private string _id;
+        LinqTest()
+        {
+            if (Random.value <= 0.5)
+                _munber = 0;
+            if(Random.value > 0.5)
+                _munber = 1;
+            if (Random.value <= 0.5)
+                _id = "Foo";
+            if (Random.value > 0.5)
+                _id = "Goo";
+        }
+    }
+    public class LinqTestSearch
+    {
+        private List<LinqTest> tests;
+        
+    }
+}
