8ea9020fcb099bf778999b6e878b3e4d514735e8
And7412 <90712704+And7412@users.noreply.github.com>
2023-01-10 17:55:40 +0300

    h/w

---
+++ Assets/RPG/Metagame/Sources/InventorySystem/View/InventaryButtonView.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace RPG.Metagame.InventorySystem.View
+{
+    public class InventaryButtonView : MonoBehaviour
+    {
+        [SerializeField]private Sprite Sprite1;
+        [SerializeField] private Image Sprite2;
+        [SerializeField] private Image ImageButton;
+
+
+    }
+}
