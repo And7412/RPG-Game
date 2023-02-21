146ea3b0a8ede84960b59d0186818c594b23b063
And7412 <90712704+And7412@users.noreply.github.com>
2023-02-21 18:32:50 +0300

    H/W

--- Assets/RPG/GameMap/Sources/GameMap.cs
+++ Assets/RPG/GameMap/Sources/GameMap.cs
@@ -3,16 +3,27 @@ using RPG.GameMap.Shop;
 using RPG.GameMap.TavernSystem;
 using RPG.Metagame.Player;
 using RPG.Shared;
+using RPG.Shared.Dialog;
 using RPG.Shared.UserData;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace RPG.GameMap
 {
     public class GameMap : SceneRunner<GameMapArgs>
     {
         [SerializeField] private PlayerConfig _playerConfig;
+        [SerializeField] private Button _quit;
+        [SerializeField] private Button _save;
         [SerializeField] private Tavern _tavern;
         [SerializeField] private Market _market;
+
+        [SerializeField] private string _textExitDialog;
+        [SerializeField] private string _textExitButtonToMainMenu;
+        [SerializeField] private string _textExitButton;
+
+        private ExitDialog _exitDialog;
+        private Player _player;
 
         protected override GameMapArgs GetTestArgs()
         {
@@ -28,12 +39,22 @@ namespace RPG.GameMap
         {
             var provider = ServiceLocator.Instance.GetService<PrefsJsonProvider>();
             var save = provider.Load<PlayerSave>();
+            _exitDialog = new ExitDialog();
 
-            var player = new Player(_playerConfig, save);
+            _player = new Player(_playerConfig, save);
 
-            player.SetMaxHP();
-            _tavern.Initialize(player);
-            _market.Initialize(player);
+            _player.SetMaxHP();
+            _tavern.Initialize(_player);
+            _market.Initialize(_player);
+        }
+
+        private void Save()
+        {
+            //var PrefProvider = ServiceLocator.Instance.GetService<PrefsJsonProvider>();
+        }
+        private void Exit()
+        {
+            _exitDialog.Open(new ExitDialogArgs(_textExitDialog,_textExitButton,_textExitButtonToMainMenu));
         }
     }
 }
---
+++ Assets/RPG/Shared/Sources/Dialog/ExitDialog.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RPG.Shared.Dialog
+{
+    public class ExitDialog : Dialog<ExitDialogArgs, ExitDialogResult>
+    {
+        [SerializeField] private TextMeshProUGUI _text;
+        [SerializeField] private Button _buttonMainMenu;
+        [SerializeField] private Button _ExitButton;
+
+        public event Action<ExitDialogResult> Closed;
+
+        protected override void OnOpen(ExitDialogArgs args)
+        {
+            _buttonMainMenu.onClick.AddListener(ToMain);
+            _ExitButton.onClick.AddListener(ToExit);
+            _text.text = args.Text;
+        }
+
+        private void ToMain()
+        {
+            Closed?.Invoke(new ExitDialogResult(false));
+        }
+        private void ToExit()
+        {
+            Closed?.Invoke(new ExitDialogResult(true));
+        }
+
+
+    }
+    public class ExitDialogArgs : DialogArgs
+    {
+        public string Text;
+        public string TextButtonMainMenu;
+        public string TextButtonExit;
+        public ExitDialogArgs(string text, string textButtonExit, string textButtonMainMenu)
+        {
+            Text = text;
+            TextButtonExit = textButtonExit;
+            TextButtonMainMenu = textButtonMainMenu;
+        }
+
+    }
+    public class ExitDialogResult : DialogResult
+    {
+        public bool ToExit { get; }
+        public ExitDialogResult(bool toExit)
+        {
+            ToExit = toExit;
+        }
+
+    }
+}
