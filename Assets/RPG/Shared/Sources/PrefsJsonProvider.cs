1fd92a75e4aa4f9701a8e9362b20c73635c0ac29
And7412 <90712704+And7412@users.noreply.github.com>
2023-03-02 17:50:56 +0300

    h/w

--- Assets/RPG/Shared/Sources/PrefsJsonProvider.cs
+++ Assets/RPG/Shared/Sources/PrefsJsonProvider.cs
@@ -1,17 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 using Object = UnityEngine.Object;
 
 namespace RPG.Shared
 {
-    public class PrefsJsonProvider :MonoBehaviour
+    public class PrefsJsonProvider : MonoBehaviour
     {
         public void Initializ()
         {
             DontDestroyOnLoad(gameObject);
         }
 
-        public T Load<T>() where T: new()
+        public T Load<T>() where T : new()
         {
             var data = PlayerPrefs.GetString(typeof(T).Name);
             T result;
@@ -27,6 +29,30 @@ namespace RPG.Shared
 
             return result;
         }
+
+        public T Load<T>(string name) where T : new()
+        {
+            var data = PlayerPrefs.GetString(name);
+            T result;
+
+            if (string.IsNullOrEmpty(data))
+            {
+                result = new T();
+            }
+            else
+            {
+                result = JsonUtility.FromJson<T>(data);
+            }
+
+            return result;
+        }
+
+        //public T LoadAllSaves<T>() where T: List<UserData.UserSave>
+        //{
+        //    var result;
+        //    //TODO
+        //    return result;
+        //}
 
         public void Save<T>(T value) where T : new()
         {
---
+++ Assets/RPG/Shared/Sources/UserData/UserSave.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace RPG.Shared.UserData
+{
+    [Serializable]
+    public class UserSave
+    {
+        public PlayerSave PlayerSave;
+        public VendorSave[] VendorSave;
+    }
+}
