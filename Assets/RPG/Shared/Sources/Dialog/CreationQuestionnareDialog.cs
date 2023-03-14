8af65d4ccac3df5919f72b7861e16f48867370e0
And7412 <90712704+And7412@users.noreply.github.com>
2023-03-14 18:33:58 +0300

    h/w

---
+++ Assets/RPG/Shared/Sources/Dialog/CreationQuestionnareDialog.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using RPG.Metagame.Player;
+using RPG.Shared.Dialog;
+
+public class CreationQuestionnareDialog : Dialog<CreationQuestionnareArgs, CreationQuestionnareResult>
+{
+    protected override void OnOpen(CreationQuestionnareArgs args)
+    {
+        
+    }
+
+    protected override void OnClose(CreationQuestionnareResult args)
+    {
+        
+    }
+}
+public class CreationQuestionnareResult : DialogResult
+{
+    public readonly PlayerAttributes playerAttributes;
+
+    public CreationQuestionnareResult(PlayerAttributes attributes)
+    {
+        playerAttributes = attributes;
+    }
+}
+public class CreationQuestionnareArgs : DialogArgs
+{
+    public string Text1;
+    public string Text2;
+    public string Text3;
+}
