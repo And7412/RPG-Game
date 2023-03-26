ad737bc3abc4f293466e79f12dbcde61e00ca238
And7412 <90712704+And7412@users.noreply.github.com>
2023-03-26 18:05:53 +0300

    h/w

--- Assets/RPG/CharacterCreation/Sources/CreateNamePlayer.cs
+++ Assets/RPG/CharacterCreation/Sources/CreateNamePlayer.cs
@@ -9,11 +9,14 @@ namespace RPG.CharacterCreation
     public class CreateNamePlayer : Dialog<DialogCreateNamePlayerArg, DialogCreateNamePlayerResult>
     {
         public TMP_Text _text;
+        public TextMeshProUGUI register_username;
+        string username;
 
         protected override void OnOpen(DialogCreateNamePlayerArg arg)
         {
             _text = arg._text;
-            
+
+            username = register_username.text;
         }
 
     }
