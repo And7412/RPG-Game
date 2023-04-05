98ecfe90c819b29270b16ac6bffccacfc6f2c449
And7412 <90712704+And7412@users.noreply.github.com>
2023-04-05 17:54:27 +0300

    UserSave updata

--- Assets/RPG/CharacterCreation/Sources/CharacterCreationSceneRunner.cs
+++ Assets/RPG/CharacterCreation/Sources/CharacterCreationSceneRunner.cs
@@ -1,13 +1,23 @@
 ﻿using RPG.Shared;
 using UnityEngine;
+using RPG.Metagame.Player;
+using RPG.Shared.UserData;
+using System.Threading.Tasks;
 
 namespace RPG.CharacterCreation
 {
     public class CharacterCreationSceneRunner : SceneRunner<CharacterCreationArgs>
     {
         [SerializeField] private CharacterCreationQuestionnare _questionnare;
-
+        [SerializeField] private Shared.SystemData.QuestDataBase _questDataBase;
         [SerializeField] private CreateNamePlayerDialog _createNameDialog;
+
+        private PlayerSave _playerSave = new PlayerSave();
+        private PlayerAttributes _playerAttributes = new PlayerAttributes(0,0,0,0,0,0,0);
+
+        public PlayerAttributes PlayerAttributes => _playerAttributes;
+
+        public UserSave _userSave;
 
         protected override CharacterCreationArgs GetTestArgs()
         {
@@ -18,6 +28,27 @@ namespace RPG.CharacterCreation
         {
             var name = await _createNameDialog.Run(new DialogCreateNamePlayerArg());
             var attributes = await _questionnare.Run();
+            foreach(AttributeMock mock in attributes)
+            {
+                _playerAttributes.AddSkill(mock.Name, mock.Value);
+            }
+            
+            
+            _userSave.Name=name.Name;
+            _userSave.PlayerSave = SetPlayerSave()
+        }
+        private PlayerSave SetPlayerSave(string name)
+        {
+            PlayerSave save;
+            save = new PlayerSave();
+            save.Name = name;
+            save.Level = 1;
+            save.Xp = 0;
+            save.Money = 100;
+            save.Difficulty = (int) Metagame.Difficulty.Medium;
+            save.Quests = new PlayerSave.Quest[_questDataBase]();
+            
+            return save;
         }
     }
 }
---
+++ Assets/RPG/Database/Quests/QuestDataBase.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static RPG.Shared.UserData.PlayerSave;
+
+namespace RPG.Shared.SystemData
+{
+    [CreateAssetMenu(menuName = "RPG/QuestsDatabase", fileName = "QuestDatabase")]
+    public class QuestDataBase : ScriptableObject
+    {
+        [SerializeField] private List<Quest> _quests;
+
+        public List<Quest> Quests => _quests;
+        private void OnValidate()
+        {
+            var ids = new List<string>();
+            var quests = new List<Quest>();
+
+            foreach (var quest in _quests)
+            {
+                if (!ids.Contains(quest.Id))
+                {
+                    quests.Add(quest);
+                    ids.Add(quest.Id);
+                }
+            }
+
+            _quests = quests;
+        }
+    }
+}
