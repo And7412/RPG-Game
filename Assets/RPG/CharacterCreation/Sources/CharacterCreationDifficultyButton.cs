d055d10a249d93cd29a8560d2b26a6c4bc0654f9
And7412 <90712704+And7412@users.noreply.github.com>
2023-04-17 21:59:52 +0300

    H/W

---
+++ Assets/RPG/CharacterCreation/Sources/CharacterCreationDifficultyButton.cs
@@ -0,0 +1,32 @@
+﻿using RPG.Metagame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RPG.CharacterCreation.DiffilcultyDialog
+{
+    [RequireComponent(typeof(Button))]
+    public class CharacterCreationDifficultyButton : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _textTMPro;
+        [SerializeField] private string _text;
+        private Button _button;
+        [SerializeField] private Difficulty _difficulty;
+        public event Action<Difficulty> Clicked;
+        public Difficulty Difficulty => _difficulty;
+
+        private void Awake()
+        {
+            _button = GetComponent<Button>();
+            _button.onClick.AddListener(OnClick);
+        }
+        private void OnClick()
+        {
+            Clicked?.Invoke(_difficulty);
+        }
+    }
+}
+
---
+++ Assets/RPG/CharacterCreation/Sources/DiffilcultyDialog.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Shared.Dialog;
+using RPG.Metagame;
+using System.Threading.Tasks;
+using TMPro;
+
+namespace RPG.CharacterCreation.DiffilcultyDialog
+{
+    public class DiffilcultyDialog : Dialog<DialogArgs, DifficultyDialogResult>
+    {
+        [SerializeField] private CharacterCreationDifficultyButton[] _buttons;
+        [SerializeField] private TMP_Text _textTMPro;
+        [SerializeField] private string _text;
+
+        protected override void OnOpen(DialogArgs args)
+        {
+            foreach(var button in _buttons)
+            {
+                button.Clicked += DifficultyButtonOnClick;
+            }
+        }
+        private void DifficultyButtonOnClick(Difficulty difficulty)
+        {
+            SetResult(new DifficultyDialogResult(difficulty));
+        }
+    }
+    public class DifficultyDialogResult : DialogResult
+    {
+        private Difficulty _difficulty;
+        public DifficultyDialogResult(Difficulty difficulty)
+        {
+            difficulty = _difficulty;
+        }
+    }
+}
