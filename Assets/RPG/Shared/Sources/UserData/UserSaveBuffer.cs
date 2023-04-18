d7794f3805bec83e89cc3daa929511ba6f675f56
And7412 <90712704+And7412@users.noreply.github.com>
2023-04-18 20:11:14 +0300

    Create UserSaveBuffer

---
+++ Assets/RPG/Shared/Sources/UserData/UserSaveBuffer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace RPG.Shared.UserData
+{
+    public class UserSaveBuffer
+    {
+        private readonly UserSaveSystem _saveSystem;
+        private readonly UserSave _save;
+
+        public UserSaveBuffer(UserSaveSystem saveSystem, UserSave save)
+        {
+            _saveSystem = saveSystem;
+            _save = save;
+        }
+
+        public void SaveLevel(int xp, int lv)
+        {
+            _save.PlayerHeroData.LevelData.Xp = xp;
+            _save.PlayerHeroData.LevelData.Level = lv;
+        }
+    }
+}
